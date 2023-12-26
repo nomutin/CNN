325b6581d167e28e4479e5c0fd0bc48ec8f7c07b
nomutin <nomura0508@icloud.com>
2023-12-27 02:50:42 +0900

    `EncoderConfig`・`DecoderConfig`をインポート

--- src/cnn/__init__.py
+++ src/cnn/__init__.py
@@ -1,10 +1,12 @@
 """CNN module core."""
 
 from cnn.module import VAE
-from cnn.network import Decoder, Encoder
+from cnn.network import Decoder, Encoder, EncoderConfig, DecoderConfig
 
 __all__ = [
     "VAE",
     "Decoder",
+    "DecoderConfig",
     "Encoder",
+    "EncoderConfig",
 ]
